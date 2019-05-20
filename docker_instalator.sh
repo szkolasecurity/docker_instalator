9f34c084dbd31bae33782d2510ecfc5b3da57df4
szkolasecurity <50802226+szkolasecurity@users.noreply.github.com>
2019-05-20 06:30:38 +0200

    Update docker_instalator.sh

--- docker_instalator.sh
+++ docker_instalator.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# skrypt na podstawie artykułu: https://medium.com/@calypsobronte/installing-docker-in-kali-linux-2018-1-ef3a8ce3648
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+
 apt-get remove docker docker-engine docker.io
 apt-get install docker-ce
 docker run hello-world
